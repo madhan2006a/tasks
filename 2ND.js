3cf485cdb6c0136a252b5233140967e79e20b7a3
madhan2006a <madhana186@gmail.com>
2025-01-22 10:33:02 +0530

    added

---
+++ 2ND.js
@@ -0,0 +1,52 @@
+console.log("hello");
+let ag=17;
+if (ag>=18) {
+    console.log("eligible for vote");
+}
+else{
+    console.log("not eligible");
+}
+let a='b';
+switch(a){
+    case 'a':
+        console.log("vowel");
+        break;
+    case 'e':
+        console.log("vowel");
+        break;
+    case 'i':
+        console.log("vowel");
+        break;
+    case 'o':
+        console.log("vowel");
+        break;
+    case 'u':
+        console.log("vowel");   
+        break;
+    case 'A':
+        console.log("vowel");
+        break;
+    case 'E':
+        console.log("vowel");
+        break;
+    case 'I':
+        console.log("vowel");
+        break;
+    case 'O':
+        console.log("vowel");
+        break;
+    case 'U':
+        console.log("vowel");   
+        break;
+ 
+    default:
+        console.log("consonant");
+    
+    }
+let b=23;
+if(b%2==0){
+    console.log("Even");    
+}
+else{
+    console.log("Odd");
+}
